0b282debf1643b96a2d8411f2c27e0b9393a9e06
Sunsnaker <cesar_pino69@hotmail.com>
2021-08-25 18:11:41 -0500

    diseño de portfolio

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleText = document.getElementById('toggle-text');
+
+const toggleColor = document.getElementById('toggle-color');
+
+toggleTheme.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    if (toggleIcon.classList.contains('fa-moon')) {
+        toggleIcon.classList.replace('fa-moon', 'fa-sun');
+        toggleText.textContent = 'Light Mode';
+    } else {
+        toggleIcon.classList.replace('fa-sun', 'fa-moon');
+        toggleText.textContent = 'Dark Mode';
+    }
+});
+
+toggleColor.addEventListener('click', (e) => {
+    if (e.target.classList.contains('colors__item')) {
+        rootStyles.setProperty("--primary-color", e.target.dataset.color)
+    }
+});
